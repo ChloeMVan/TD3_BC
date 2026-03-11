a9914f12a4ec56aac7525b2ac70790b13b2bc49c
Divya-Joshi08 <joshidivya08@gmail.com>
2026-03-11 00:41:16 -0500

    hello, run_o2o.sh to run the experiments, i also made the number of timesteps really small so it would finsh

--- utils.py
+++ utils.py
@@ -29,7 +29,7 @@ class ReplayBuffer(object):
 
 
 	def sample(self, batch_size):
-		ind = np.random.randint(0, self.size, size=batch_size)
+		ind = np.random.randint(0, len(self.state), size=batch_size)
 
 		return (
 			torch.FloatTensor(self.state[ind]).to(self.device),
