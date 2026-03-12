5428ddeea97459bcfe5eed1b780ea59abb56d502
Divya-Joshi08 <joshidivya08@gmail.com>
2026-03-12 01:16:07 -0500

    idkkk

--- o2o_old.py
+++ o2o_old.py
@@ -11,29 +11,9 @@ import TD3_BC
 import TD3
 
 
-def eval_online_policy(policy, env_name, seed, eval_episodes=10):
-	eval_env = gym.make(env_name)
-	eval_env.seed(seed + 100)
-
-	avg_reward = 0.
-	for _ in range(eval_episodes):
-		state, done = eval_env.reset(), False
-		while not done:
-			action = policy.select_action(np.array(state))
-			state, reward, done, _ = eval_env.step(action)
-			avg_reward += reward
-
-	avg_reward /= eval_episodes
-
-	print("---------------------------------------")
-	print(f"Evaluation over {eval_episodes} episodes: {avg_reward:.3f}")
-	print("---------------------------------------")
-	return avg_reward
-
-
 # Runs policy for X episodes and returns D4RL score
 # A fixed seed is used for the eval environment
-def eval_offline_policy(policy, env_name, seed, mean, std, seed_offset=100, eval_episodes=10):
+def eval_policy(policy, env_name, seed, mean, std, seed_offset=100, eval_episodes=10):
 	eval_env = gym.make(env_name)
 	eval_env.seed(seed + seed_offset)
 
@@ -56,13 +36,11 @@ def eval_offline_policy(policy, env_name, seed, mean, std, seed_offset=100, eval
 
 
 if __name__ == "__main__":
-	
+
 	parser = argparse.ArgumentParser()
 	# Experiment
 	parser.add_argument("--policy", default="TD3_BC")               # Policy name
 	parser.add_argument("--env", default="hopper-medium-v0")        # OpenAI gym environment name
-	parser.add_argument("--offline_env", default="hopper-medium-v0")        # OpenAI gym environment name
-	parser.add_argument("--online_env", default="Hopper-v3")        # online environemnt 
 	parser.add_argument("--seed", default=0, type=int)              # Sets Gym, PyTorch and Numpy seeds
 	parser.add_argument("--eval_freq", default=5e3, type=int)       # How often (time steps) we evaluate
 	parser.add_argument("--max_timesteps", default=15000, type=int)   # Max time steps to run environment
@@ -76,7 +54,6 @@ if __name__ == "__main__":
 	parser.add_argument("--policy_noise", default=0.2)              # Noise added to target policy during critic update
 	parser.add_argument("--noise_clip", default=0.5)                # Range to clip target policy noise
 	parser.add_argument("--policy_freq", default=2, type=int)       # Frequency of delayed policy updates
-	parser.add_argument("--start_timesteps", default=0, type=int)# Time steps initial random policy is used
 	# TD3 + BC
 	parser.add_argument("--alpha", default=2.5)
 	parser.add_argument("--normalize", default=True)
@@ -100,7 +77,7 @@ if __name__ == "__main__":
 	env.action_space.seed(args.seed)
 	torch.manual_seed(args.seed)
 	np.random.seed(args.seed)
-	
+
 	state_dim = env.observation_space.shape[0]
 	action_dim = env.action_space.shape[0] 
 	max_action = float(env.action_space.high[0])
@@ -132,25 +109,27 @@ if __name__ == "__main__":
 		mean,std = replay_buffer.normalize_states() 
 	else:
 		mean,std = 0,1
-	
+
+	print("offline")
 	# ----------------------- offline -----------------------------------
-	print("STARTING OFFLINE TRAINING")
+	print("OFFLINE: EXACT SAME AS MAIN.PY")
 	evaluations = []
 	for t in range(int(args.max_timesteps)):
 		policy.train(replay_buffer, args.batch_size)
 		# Evaluate episode
 		if (t + 1) % args.eval_freq == 0:
-			print(f"[Offline] Time steps: {t+1}")
-			evaluations.append(eval_offline_policy(policy, args.env, args.seed, mean, std))
+			print(f"Time steps: {t+1}")
+			evaluations.append(eval_policy(policy, args.env, args.seed, mean, std))
 			np.save(f"./results/{file_name}", evaluations)
 			if args.save_model: 
 				policy.save(f"./models/{file_name}")
-	
-	# ------------------------- online add here-----------------------------
-	print("STARTING ONLINE TRAINING") 
 
-	env = gym.make(args.env)
-	online_buffer = utils.ReplayBuffer(state_dim, action_dim)
+	# ------------------------- online ------------------------------------
+	print("Starting ONLINE training") 
+	state, done = env.reset(), False
+	state = (np.array(state).reshape(1,-1) - mean)/std
+
+	online_steps = int(args.max_timesteps)
 	kwargs = {
 		"state_dim": state_dim,
 		"action_dim": action_dim,
@@ -171,53 +150,42 @@ if __name__ == "__main__":
 	td3policy.actor_target.load_state_dict(policy.actor_target.state_dict())
 	td3policy.critic_target.load_state_dict(policy.critic_target.state_dict())
 
-	evaluations = [eval_online_policy(td3policy, args.env, args.seed)]
-
+	for t in range(online_steps):
 	state, done = env.reset(), False
-	episode_reward = 0
-	episode_timesteps = 0
-	episode_num = 0
+	state = (np.array(state).reshape(1,-1) - mean)/std
 
 	for t in range(int(args.max_timesteps)):
-		
-		episode_timesteps += 1
-
-		# Select action according to policy
-		# if t < args.start_timesteps:
-		# 	action = env.action_space.sample()
-		# else:
-		# 	norm_state = (np.array(state).reshape(1,-1) - mean) / std
+		# Select action
+		action = policy.select_action(state)
+		action = td3policy.select_action(state)
 		action = (
-			td3policy.select_action(np.array(state))
+			action
 			+ np.random.normal(0, max_action * args.expl_noise, size=action_dim)
 		).clip(-max_action, max_action)
 
-		# Perform action
-		next_state, reward, done, _ = env.step(action) 
-		# done_bool = float(done) if episode_timesteps < env._max_episode_steps else 0
+		next_state, reward, done, _ = env.step(action)
+		next_state_norm = (np.array(next_state).reshape(1,-1) - mean)/std
 
-		# Store data in replay buffer
-		norm_next_state = (np.array(next_state).reshape(1,-1) - mean) / std
-		online_buffer.add(state, action, next_state, reward, done)
+		# Store new transition
+		replay_buffer.add(state, action, next_state_norm, reward, done)
 
-		state = norm_next_state
-		episode_reward += reward
+		# Train policy
+		policy.train(replay_buffer, args.batch_size)
+		td3policy.train(replay_buffer, args.batch_size)
 
-		# Train agent 
-		# if t >= args.start_timesteps:
-		td3policy.train(online_buffer, args.batch_size)
+		state = next_state_norm
 
-		# if done: 
-		# 	# +1 to account for 0 indexing. +0 on ep_timesteps since it will increment +1 even if done=True
-		# 	print(f"Total T: {t+1} Episode Num: {episode_num+1} Episode T: {episode_timesteps} Reward: {episode_reward:.3f}")
-		# 	# Reset environment
-		# 	state, done = env.reset(), False
-		# 	episode_reward = 0
-		# 	episode_timesteps = 0
-		# 	episode_num += 1 
+		if done:
+			state, done = env.reset(), False
+			state = (np.array(state).reshape(1,-1) - mean)/std
 
-		# Evaluate episode
+		# Evaluation
 		if (t + 1) % args.eval_freq == 0:
-			evaluations.append(eval_offline_policy(td3policy, args.env, args.seed, mean, std))
+			print(f"[Online] Time steps: {t+1}")
+			evaluations.append(eval_policy(policy, args.env, args.seed, mean, std))
+			evaluations.append(eval_policy(td3policy, args.env, args.seed, mean, std))
 			np.save(f"./results/{file_name}", evaluations)
-			if args.save_model: td3policy.save(f"./models/{file_name}")
+
+			if args.save_model:
+				policy.save(f"./models/{file_name}_online")
+				td3policy.save(f"./models/{file_name}_online")
