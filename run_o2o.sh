f362d9c565412b28fb7838735347b0e29e85a2b8
ChloeMVan <74740805+ChloeMVan@users.noreply.github.com>
2026-04-15 01:16:44 -0500

    m

--- run_o2o.sh
+++ run_o2o.sh
@@ -3,7 +3,21 @@
 # Script to reproduce results
 
 envs=(
-	"halfcheetah-expert-v2"
+	# "halfcheetah-random-v0"
+	# "hopper-random-v0"
+	"walker2d-random-v2"
+	# "halfcheetah-medium-v0"
+	# "hopper-medium-v0"
+	# "walker2d-medium-v2" *
+	# "halfcheetah-expert-v0"
+	# "hopper-expert-v0"
+	# "walker2d-expert-v2" *
+	# "halfcheetah-medium-expert-v0"
+	# "hopper-medium-expert-v0"
+	# "walker2d-medium-expert-v2" *
+	# "halfcheetah-medium-replay-v0"
+	# "hopper-medium-replay-v0"
+	# "walker2d-medium-replay-v0"
 	)
 
 for ((i=0;i<1;i+=1))
