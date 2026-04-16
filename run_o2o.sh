33375ea77bf97b47f190ca259f3d9ebde87ae5a2
ChloeMVan <74740805+ChloeMVan@users.noreply.github.com>
2026-04-15 22:04:19 -0500

    setting up ME

--- run_o2o.sh
+++ run_o2o.sh
@@ -11,10 +11,10 @@ envs=(
 	# "walker2d-medium-v2"
 	# "halfcheetah-expert-v0"
 	# "hopper-expert-v0"
-	"walker2d-expert-v2" *
+	# "walker2d-expert-v2" *
 	# "halfcheetah-medium-expert-v0"
 	# "hopper-medium-expert-v0"
-	# "walker2d-medium-expert-v2" *
+	"walker2d-medium-expert-v2"
 	# "halfcheetah-medium-replay-v0"
 	# "hopper-medium-replay-v0"
 	# "walker2d-medium-replay-v0"
