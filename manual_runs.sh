b4c9f014e3c89ffcbbd6cf80125c6583d349b634
ChloeMVan <74740805+ChloeMVan@users.noreply.github.com>
2026-04-17 10:55:32 -0500

    added manual runs

--- manual_runs.sh
+++ manual_runs.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 python3 o2o_BC.py --file_tag 'BC_Fast' --env 'halfcheetah-expert-v2' --root_num 2
 
+python3 o2o_BC.py --file_tag 'BC_Mid' --env 'halfcheetah-expert-v2' --root_num 2.3
+
 python3 o2o_BC.py --file_tag 'BC_Slow' --env 'halfcheetah-expert-v2' --root_num 2.5
 
 python3 o2o_BC.py --file_tag 'BC_Fast' --env 'halfcheetah-medium-expert-v2' --root_num 2
