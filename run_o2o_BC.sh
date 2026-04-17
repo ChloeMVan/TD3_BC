cabf6e868865f708055d4eb57a835edc75d2b079
ChloeMVan <74740805+ChloeMVan@users.noreply.github.com>
2026-04-17 01:34:12 -0500

    medium setup for fast

--- run_o2o_BC.sh
+++ run_o2o_BC.sh
@@ -2,14 +2,16 @@
 
 # Script to reproduce results
 
+# Fast - 2, Mid - 2.3, Slow - 2.5
+
 envs=(
 	# "halfcheetah-random-v0"
 	# "hopper-random-v0"
 	# "walker2d-random-v2"
-	# "halfcheetah-medium-v0"
+	"halfcheetah-medium-v2"
 	# "hopper-medium-v0"
 	# "walker2d-medium-v2"
-	"halfcheetah-expert-v2"
+	# "halfcheetah-expert-v2"
 	# "hopper-expert-v0"
 	# "walker2d-expert-v2"
 	# "halfcheetah-medium-expert-v0"
@@ -25,9 +27,9 @@ do
 	for env in ${envs[*]}
 	do
 		python o2o_BC.py \
-        --file_tag 'BC_Slow' \
+        --file_tag 'BC_Fast' \
 		--env $env \
 		--seed $i \
-		--root_num 2.5
+		--root_num 2
 	done
 done
