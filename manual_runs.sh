573ece8e7eaccf71db8e6bdb5fd7a694b40f9258
ChloeMVan <74740805+ChloeMVan@users.noreply.github.com>
2026-04-19 00:27:47 -0500

    should I just die

--- manual_runs.sh
+++ manual_runs.sh
@@ -1,39 +1,20 @@
 #!/bin/bash
 
+# nohup bash -c 'cd /home/chloe/TD3_BC; source /home/chloe/miniconda3/etc/profile.d/conda.sh; conda activate td3_env; export TMPDIR=/home/chloe/tmp; bash manual_runs.sh' > test_run.log 2>&1 &
+
 # test
-python3 o2o_BC.py --file_tag 'Test_BC_Fast' --env 'halfcheetah-expert-v2' --root_num 0.5
 
-python3 o2o_BC.py --file_tag 'Test_BC_Mid' --env 'halfcheetah-expert-v2' --root_num 1.5
+python3 o2o_BC.py --file_tag 'First_BC_Exp' --function_type 'exp' --env 'halfcheetah-medium-v2' --param_num 2.3
 
-python3 o2o_BC.py --file_tag 'Test_BC_Slow' --env 'halfcheetah-expert-v2' --root_num 2.5
+python3 o2o_BC.py --file_tag 'First_BC_Mid_Linear' --function_type 'linear' --env 'halfcheetah-medium-v2'
+
+python3 o2o_BC.py --file_tag 'First_BC_Slow_Log' --function_type 'log' --env 'halfcheetah-medium-v2'
+
+python3 o2o_BC.py --file_tag 'First_BC_Exp' --function_type 'exp' --env 'halfcheetah-expert-v2' --param_num 2.3
+
+python3 o2o_BC.py --file_tag 'First_BC_Mid_Linear' --function_type 'linear' --env 'halfcheetah-expert-v2'
+
+python3 o2o_BC.py --file_tag 'First_BC_Slow_Log' --function_type 'log' --env 'halfcheetah-expert-v2'
 
 
-python3 o2o_BC.py --file_tag 'Test_BC_Mid_Linear' --function_type 'linear' --env 'halfcheetah-expert-v2'
-
-python3 o2o_BC.py --file_tag 'Test_BC_Slow_Log' --function_type 'log' --env 'halfcheetah-expert-v2'
-
-nohup bash -c 'cd /home/chloe/TD3_BC; source /home/chloe/miniconda3/etc/profile.d/conda.sh; conda activate td3_env; export TMPDIR=/home/chloe/tmp; python3 o2o_BC.py --file_tag "Test_BC_Mid_Linear" --function_type "linear" --env "halfcheetah-expert-v2"; python3 o2o_BC.py --file_tag "Test_BC_Slow_Log" --function_type "log" --env "halfcheetah-expert-v2"' > test_run.log 2>&1 &
-
-# prior
-python3 o2o_BC.py --file_tag 'BC_Fast' --env 'halfcheetah-expert-v2' --root_num 2
-
-python3 o2o_BC.py --file_tag 'BC_Mid' --env 'halfcheetah-expert-v2' --root_num 2.3
-
-python3 o2o_BC.py --file_tag 'BC_Slow' --env 'halfcheetah-expert-v2' --root_num 2.5
-
-python3 o2o_BC.py --file_tag 'BC_Fast' --env 'halfcheetah-medium-expert-v2' --root_num 2
-
-python3 o2o_BC.py --file_tag 'BC_Mid' --env 'halfcheetah-medium-expert-v2' --root_num 2.3
-
-python3 o2o_BC.py --file_tag 'BC_Slow' --env 'halfcheetah-medium-expert-v2' --root_num 2.5
-
-python3 o2o_BC.py --file_tag 'BC_Fast' --env 'halfcheetah-medium-v2' --root_num 2
-
-python3 o2o_BC.py --file_tag 'BC_Mid' --env 'halfcheetah-medium-v2' --root_num 2.3
-
-python3 o2o_BC.py --file_tag 'BC_Slow' --env 'halfcheetah-medium-v2' --root_num 2.5
-
-python3 o2o.py --file_tag 'o2o' --env 'halfcheetah-expert-v2'
-
-python3 online.py --file_tag 'org' --env 'walker2d-random-v2' 
 
