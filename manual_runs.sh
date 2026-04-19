eb7a1a88278b1ee9537f7a263290de13fec60203
ChloeMVan <74740805+ChloeMVan@users.noreply.github.com>
2026-04-19 11:45:18 -0500

    saved results, setting up hopper

--- manual_runs.sh
+++ manual_runs.sh
@@ -4,17 +4,29 @@
 
 # test
 
-python3 o2o_BC.py --file_tag 'First_BC_Exp' --function_type 'exp' --env 'halfcheetah-medium-v2' --param_num 2.3
+# python3 o2o_BC.py --file_tag 'First_BC_Exp' --function_type 'exp' --env 'halfcheetah-medium-v2' --param_num 2.3
 
-python3 o2o_BC.py --file_tag 'First_BC_Mid_Linear' --function_type 'linear' --env 'halfcheetah-medium-v2'
+# python3 o2o_BC.py --file_tag 'First_BC_Mid_Linear' --function_type 'linear' --env 'halfcheetah-medium-v2'
 
-python3 o2o_BC.py --file_tag 'First_BC_Slow_Log' --function_type 'log' --env 'halfcheetah-medium-v2'
+# python3 o2o_BC.py --file_tag 'First_BC_Slow_Log' --function_type 'log' --env 'halfcheetah-medium-v2'
 
 # python3 o2o_BC.py --file_tag 'First_BC_Exp' --function_type 'exp' --env 'halfcheetah-expert-v2' --param_num 2.3
 
-python3 o2o_BC.py --file_tag 'First_BC_Mid_Linear' --function_type 'linear' --env 'halfcheetah-expert-v2'
+# python3 o2o_BC.py --file_tag 'First_BC_Mid_Linear' --function_type 'linear' --env 'halfcheetah-expert-v2'
 
 # python3 o2o_BC.py --file_tag 'First_BC_Slow_Log' --function_type 'log' --env 'halfcheetah-expert-v2'
 
 
+
+python3 o2o_BC.py --file_tag 'First_BC_Exp' --function_type 'exp' --env 'hopper-expert-v2' --param_num 2.3
+
+python3 o2o_BC.py --file_tag 'First_BC_Mid_Linear' --function_type 'linear' --env 'hopper-expert-v2'
+
+python3 o2o_BC.py --file_tag 'First_BC_Slow_Log' --function_type 'log' --env 'hopper-expert-v2'
+
+python3 o2o_BC.py --file_tag 'First_BC_Exp' --function_type 'exp' --env 'hopper-medium-v2' --param_num 2.3
+
+python3 o2o_BC.py --file_tag 'First_BC_Mid_Linear' --function_type 'linear' --env 'hopper-medium-v2'
+
+python3 o2o_BC.py --file_tag 'First_BC_Slow_Log' --function_type 'log' --env 'hopper-medium-v2'
 
